da10f856748f4c12d215984df83c0103c90aeb7f
Balog Bence <balogbence991@gmail.com>
2024-02-13 14:30:32 +0100

    Kezdet

--- src/main/java/com/example/keringo/HelloApplication.java
+++ src/main/java/com/example/keringo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.keringo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Keringő");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("ball1.png")));
         stage.setScene(scene);
         stage.show();
     }
