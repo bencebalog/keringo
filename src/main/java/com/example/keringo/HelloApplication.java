5e3a3f2fcf829a9d9878c484d321e8a6bd0c7a84
Balog Bence <balogbence991@gmail.com>
2024-02-13 14:40:41 +0100

    GUI

--- src/main/java/com/example/keringo/HelloApplication.java
+++ src/main/java/com/example/keringo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Keringő");
         stage.getIcons().add(new Image(getClass().getResourceAsStream("ball1.png")));
         stage.setScene(scene);
